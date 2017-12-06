98cc384eb1585b1eac519b928069ba3e8c41ea1a
Jelvibean <yelvyb@yahoo.com>
2017-12-05 22:59:45 -0500

    adding new class placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
 
 const contentNode = document.getElementById('contents');
-
-const continents = ['Africa','America','Asia','Australia','Europe'];
-
-
-const message = continents.map(c => `Hello ${c}!`).join(' ');
+const contentNode2 = document.getElementById('contents2');
 
 
-const component = <p>{message}</p>;        // A simple component, written in JSX
-ReactDOM.render(component, contentNode);      // Render the component inside the content Node
+class IssueFilter extends React.Component{
+	render() {
+		return (
+			<div>This is a place holder for the Issue Filter</div>
+		);
+	}
+}
+
+class IssueTable extends React.Component {
+	render(){
+		return (
+			<div>This is a place holder for a table of Issues</div>
+		);
+	}
+}
+
+class IssueAdd extends React.Component{
+	render() {
+		return (
+			<div>This is a place holder for an Issue Add Entry form</div>
+		);
+	}
+}
+
+class IssueList extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Issue Tracker</h1>
+				<IssueFilter/>
+				<hr/>
+				<IssueTable/>
+				<hr/>
+				<IssueAdd/>
+			</div>	
+		);
+	}
+}
 
 
+const testthis = <h1>This shows that native elements like an h1s dont need to be instantiated</h1>;
+ReactDOM.render(testthis, contentNode2);   //This is to show that a div or h1 are built in internal React componetns that can be directly instantiated
+
+
+ReactDOM.render(<IssueList/>, contentNode);  //Render the component inside the content node
 
